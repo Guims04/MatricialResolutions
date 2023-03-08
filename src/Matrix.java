23f8435354b23f60f89781e928d4bc7d1e39063a
guims04 <gem.epifanio@gmail.com>
2023-03-07 22:01:34 -0300

    pequena alteração no constructor da Matrix

--- src/Matrix.java
+++ src/Matrix.java
@@ -3,6 +3,13 @@ public class Matrix {
 
   public void constructor(int rows, int cols, double[] elements){
     matriz = new double[rows][cols];
+    int p = 0;
+    for (int i = 0; i < rows; i++){
+      for (int j = 0; j < cols; j++){
+        matriz[i][j] = elements[p];
+        p++;
+      }
+    }
   }
 
   public double get(int i, int j){
