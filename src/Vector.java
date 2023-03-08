1dedbf37d6a7e2239804e88fcffc9fb79ab62acd
guims04 <gem.epifanio@gmail.com>
2023-03-08 19:57:08 -0300

    pequenas atualizações

--- src/Vector.java
+++ src/Vector.java
@@ -1,15 +1,15 @@
 public class Vector {
-  double[] vector;
+  double[][] vector;
 
   public void constructor(int dim, double[] elements){
-    vector = new double[dim];
+    vector = new double[dim][1];
   }
 
   public double get(int i){
-    return vector[i];
+    return vector[i][1];
   }
 
   public void set(int i, double value){
-    vector[i] = value;
+    vector[i][1] = value;
   }
 }
